fa29eb4e0425a983172847b5bf6ed9983f5f9a32
Emilio <emilioplatzer@gmail.com>
2018-04-24 13:44:43 -0300

    v0.1.18

--- lib/client/form-structure.d.ts
+++ lib/client/form-structure.d.ts
@@ -199,7 +199,7 @@ export declare class FormManager {
     validateDepot(): void;
     consistencias(): void;
     refreshState(): void;
-    posicionarVentanaVerticalmente(control: HTMLElement, y: number): any;
+    posicionarVentanaVerticalmente(control: HTMLElement, y: number): number;
     irAlSiguiente(variableActual: string, scrollScreen: boolean): void;
     completarHora(value: any): any;
 }
