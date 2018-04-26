3f18727797e07f1f26d4ff0d7b2009283fbbf808
Emilio <emilioplatzer@gmail.com>
2018-04-26 13:45:42 -0300

    agregamos decimal a formTypes

--- lib/client/form-types.d.ts
+++ lib/client/form-types.d.ts
@@ -1,7 +1,7 @@
 export declare var formTypes: {
     [key: string]: {
         htmlType: 'text' | 'number';
-        typeName: 'bigint' | 'text';
+        typeName: 'bigint' | 'text' | 'decimal';
         validar: 'texto' | 'opciones' | 'numerico';
         radio?: boolean;
     };
--- src/server/rel-enc.ts
+++ src/server/rel-enc.ts
@@ -1,5 +1,7 @@
 import * as backendPlus from "backend-plus";
 
+export * from "../client/form-types";
+
 var x =  new backendPlus.AppBackend();
 
 export class AppRelEnc extends backendPlus.AppBackend{
