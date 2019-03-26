39d83ed1f149281947b336771101e0cb9b9f99f7
Manuel De la Penna <manueldelapenna@gmail.com>
2019-03-26 12:10:21 -0300

    actualizo estructura rel-enc

--- src/server/server-rel-enc.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var rel_enc_1 = require("./rel-enc");
-var operativos_1 = require("operativos");
-var AppRelEnc = rel_enc_1.emergeAppRelEnc(operativos_1.emergeAppOperativos(operativos_1.AppBackend));
-new AppRelEnc().start();
